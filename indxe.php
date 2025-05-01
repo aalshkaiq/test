03b1202984b8af5bdabc97519c1728298890043f
aalshkaiq <aalshka.iq@gmail.com>
2025-05-02 01:15:19 +0300

    Update indxe.php

--- indxe.php
+++ indxe.php
@@ -1,3 +1,11 @@
 <?php
+$botToken = "YOUR_BOT_TOKEN";
+$update = json_decode(file_get_contents("php://input"), true);
 
-echo "hi";
+if (isset($update["message"]["text"])) {
+    $chatId = $update["message"]["chat"]["id"];
+    $messageText = $update["message"]["text"];
+
+    file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=Hello+from+Render!");
+}
+?>
